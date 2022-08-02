34892b0fe2727e1bc330592ba8cb07d2e9346040
ondrej.lostak@datasentics.com <ondrej.lostak@datasentics.com>
2022-08-02 11:12:58 +0000

    Python workshop - Pandas complete

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -15,6 +15,18 @@ import pandas as pd
 
 # COMMAND ----------
 
+source_tips = "./data/tips.csv"
+source_titanic = "./data/titanic.csv"
+source_iris = "./data/iris.csv"
+source_air_quality_no2 = "./data/air_quality_pm25_long.csv"
+source_air_quality_pm25 = "./data/air_quality_no2_long.csv"
+
+df_iris = pd.read_csv(source_iris)
+air_quality_pm25 = pd.read_csv(source_air_quality_pm25)
+air_quality_no2 = pd.read_csv(source_air_quality_no2)
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## DataFrame
 # MAGIC - data in table representation
@@ -23,31 +35,30 @@ import pandas as pd
 # COMMAND ----------
 
 # DBTITLE 1,Read CSV to DataFrame
-df_titanic = pd.read_csv("/dbfs/FileStore/user/ondrej.lostak@datasentics.cz/TitanicData.csv")
-df_titanic
+df_tips = pd.read_csv("./data/tips.csv")
+df_tips
 
 # COMMAND ----------
 
 # DBTITLE 1,Display first N rows
-df_titanic.head(5)
+df_tips.head(5)
 
 # COMMAND ----------
 
 # DBTITLE 1,Display schema
-# df_titanic.dtypes
-df_titanic.dtypes
+df_tips.dtypes
 
 # COMMAND ----------
 
 # DBTITLE 1,Display size
 # how to find table size (nrows, ncolumns)
-df_titanic.shape
+df_tips.shape
 
 # COMMAND ----------
 
 # DBTITLE 1,Write CSV file from DataFrame
-df_titanic.to_csv("/dbfs/FileStore/user/ondrej.lostak@datasentics.cz/titanic.csv")
-df_from_csv = pd.read_csv("/dbfs/FileStore/user/ondrej.lostak@datasentics.cz/titanic.csv")
+df_tips.to_csv("./data/tips_new.csv")
+df_from_csv = pd.read_csv("./data/tips_new.csv")
 df_from_csv
 
 # COMMAND ----------
@@ -62,10 +73,6 @@ df_from_csv
 
 # COMMAND ----------
 
-source_path = "/dbfs/FileStore/user/ondrej.lostak@datasentics.cz/TitanicData.csv"
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ## Subsets
 
@@ -73,88 +80,131 @@ source_path = "/dbfs/FileStore/user/ondrej.lostak@datasentics.cz/TitanicData.csv
 
 # DBTITLE 1,Get one column
 # get one column
-ages = df_titanic["Age"]
-ages
+total = df_tips["total_bill"]
+total
 
 # COMMAND ----------
 
 # DBTITLE 1,Get subset of columns
 # get new DataFrame with subset of columns
-# SQL: SELECT Name, Sex, Age FROM titanic 
-passengers = df_titanic[["Name", "Sex", "Age"]]
-passengers
+# SQL: SELECT tip, day, time FROM df_tips 
+tips_daytime = df_tips[["tip", "day", "time"]]
+tips_daytime
 
 # COMMAND ----------
 
 # DBTITLE 1,Filter rows
 # select files where is condition True
 # allowed operations (>, <, ==, >=, <=, !=, .isin([]), .notna()...), logical operations (|, &)
-# SQL: SELECT * FROM df_titanic WHERE Age > 35 AND Sex == "female"
-filtered_passengers = df_titanic[(df_titanic["Age"] > 35) & (df_titanic["Sex"] == "female")]
-filtered_passengers
+# SQL: SELECT * FROM df_tips WHERE tip > 3 AND time == "Lunch"
+filtered_tips = df_tips[(df_tips["tip"] > 3) & (df_tips["time"] == "Lunch")]
+filtered_tips
 
 # COMMAND ----------
 
-# DBTITLE 1,Remove NaN values and IN
+# DBTITLE 1,Filter NaN values and IN
 # SQL: SELECT * FROM df_titanic WHERE Cabin IS NOT NULL Embarked IN (C, S)
-df_titanic[(df_titanic["Cabin"].notna()) & (df_titanic["Embarked"].isin(["C", "S"]))]
+df_tips[(df_tips["total_bill"].notna()) & (df_tips["day"].isin(["Sun", "Sat"]))]
+
+# COMMAND ----------
+
+# DBTITLE 1,Drop NaN
+df = pd.read_csv(source_tips)
+df.iloc[0,0] = None
+
+# COMMAND ----------
+
+# DBTITLE 1,Check null values
+df.isnull().any()
+
+# COMMAND ----------
+
+df.dropna()
 
 # COMMAND ----------
 
 # DBTITLE 1,Access specific rows and columns by condition
-adult_women = df_titanic.loc[df_titanic["Age"] > 18, ["Name", "Age"]]
-adult_women
+families = df_tips.loc[df_tips["size"] > 2, ["day", "time", "size"]]
+families
 
 # COMMAND ----------
 
 # DBTITLE 1,Access specific cells by index
-df_titanic.iloc[0:25, 0:4]
+df_tips.iloc[0:25, 0:4]
 
 # COMMAND ----------
 
 # DBTITLE 1,Add new column
-df_titanic["Adult"] = df_titanic["Age"] > 18
-df_titanic.head(30)
+df_tips["family"] = df_tips["size"] > 2
+df_tips.head(30)
 
 # COMMAND ----------
 
 # DBTITLE 1,Rename columns
-df_renamed = df_titanic.rename(columns={"Parch": "Parents", "SibSp": "Siblings"})
+df_renamed = df_tips.rename(columns={"time": "meal_type"})
 df_renamed
 
 # COMMAND ----------
 
 # DBTITLE 1,Combine data from multiple tables
-pd.concat([air_quality_pm25, air_quality_no2], keys=["PM25", "NO2"])
+pd.concat([air_quality_pm25, air_quality_no2])
 
 # COMMAND ----------
 
 # DBTITLE 1,Join tables on common attribute
-pd.merge(air_quality, stations_coord, how="left", on="location")
+no2 = air_quality_no2[["date.utc", "value", "unit"]].rename(columns={"value": "no2", "unit": "unit_no2"})
+pm25 = air_quality_pm25.rename(columns={"value": "pm25", "unit": "unit_pm25"})
+pd.merge(pm25, no2, how="left", on="date.utc")
 
 # COMMAND ----------
 
 # DBTITLE 1,Aggregating statistics
-titanic["Age"].mean()
+df_tips["tip"].mean()
 
 # COMMAND ----------
 
-titanic[["Age", "Fare"]].median()
+df_tips[["size", "tip"]].median()
 
 # COMMAND ----------
 
-titanic[["Age", "Fare"]].describe()
+df_tips[["size", "tip"]].describe()
 
 # COMMAND ----------
 
-titanic.agg(
+df_tips.agg(
     {
-        "Age": ["min", "max", "median", "skew"],
-        "Fare": ["min", "max", "median", "mean"],
+        "size": ["min", "max", "median"],
+        "tip": ["min", "max", "median", "mean"],
     }
 )
 
 # COMMAND ----------
 
 # DBTITLE 1,Group by
+df_tips[["time", "tip"]].groupby("time").mean()
 
+# COMMAND ----------
+
+df_tips.groupby("time").mean()
+
+# COMMAND ----------
+
+df_tips.groupby(["day", "time"]).mean()
+
+# COMMAND ----------
+
+df_tips["day"].value_counts()
+
+# COMMAND ----------
+
+df_iris
+
+# COMMAND ----------
+
+# DBTITLE 1,Basic plot
+df_iris.plot()
+
+# COMMAND ----------
+
+# DBTITLE 1,Boxplot
+df_iris.plot.box()
